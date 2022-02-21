9f54726d568cf1c74d5240d86f34d3b3de246684
Mikhail Mikhailov <thebestmihmihever@gmail.com>
2022-02-21 15:43:47 +0300

    Games and Menu models

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import UserModel
+from .models import UserModel, MenuModel, GamesModel
 
 
 admin.site.register(UserModel)
+admin.site.register(MenuModel)
+admin.site.register(GamesModel)
--- base/models.py
+++ base/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
+from  djmoney.models.fields import MoneyField
 
 
 class UserModel(AbstractUser):
+    '''
+    Модель регистрации
+
+    '''
     name = models.CharField(max_length=200, null=True)
-    email = models.EmailField(unique=True, null=True)
-    phone = PhoneNumberField(unique=True, null=True, blank=True)
+    email = models.EmailField(unique=True, null=True, blank=True) #Кто сейчас регестрируется по email? Но пусть будет
+    phone = PhoneNumberField(unique=True, null=True)
 
     PHONENUMBER_DEFAULT_REGION='RU'
     USERNAME_FIELD = 'phone'
@@ -14,3 +19,32 @@ class UserModel(AbstractUser):
 
     def __str__(self):
         return str(self.phone)
+
+
+class MenuModel(models.Model):
+    '''
+    Меню бара
+
+    '''
+    product_name = models.CharField(max_length=200, null=True)
+    price = MoneyField(max_digits=14, default_currency='RUB')
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.product_name
+
+
+class GamesModel(models.Model):
+    '''
+    Модель для игр
+    game_type определяет настольная игра или для playstation
+
+    '''
+    game_name = models.CharField(max_length=200, null=True)
+    game_type = models.CharField(max_length=50, null=True)
+    bio = models.TextField(null=True, blank=True)
+
+    image = models.ImageField(null=True, default='avatar.svg')
+
+    def __str__(self):
+        return self.game_name
